31e26d05b3e45d600004b0d6e26c9fdc6b84d47b
Diga Widyaprana <digawp@gmail.com>
2016-10-14 22:07:09 +0800

    Add branched sample to escape.c

--- samples/escape.c
+++ samples/escape.c
@@ -1,11 +1,39 @@
 #include <stdlib.h>
 
+int magic() {
+	return 1;
+}
+
 char* generate_cp() {
 	char* hip = malloc(10);
-	char a = 'a';
-	char arr[3];
-	char* ptr = &a;
-	return ptr;
+	if(!magic()) {
+		return hip;
+	} else {
+		char a = 'a';
+		char arr[3];
+		char* ptr = &a;
+		return ptr;
+	}
+}
+
+char* init_array(void) {
+	char array[10];
+	char* p = array;
+	if (magic()) {
+		char a = 'b';
+		char* q = &a;
+		return q;
+	} else {
+		char* r = p;
+	return r;
+	}
+}
+
+char init_primitive(void) {
+	char p = 'a';
+	char q = p;
+	char r = q;
+	return r;
 }
 
 int main(int argc, char const *argv[]) {
